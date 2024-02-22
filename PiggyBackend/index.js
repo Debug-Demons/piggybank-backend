621ed212c4dfc56d1f369a28919a44cca7746ed7
J-robinson11 <104178757+J-robinson11@users.noreply.github.com>
2024-02-22 17:06:21 -0600

    nodemon added

--- PiggyBackend/index.js
+++ PiggyBackend/index.js
@@ -1,7 +1,29 @@
 // Import express using ESM syntax
 import express from 'express';
-import { authenticateUser } from './authMiddleware.js';
-import authRoutes from './api/auth.js';
+//import { authenticateUser } from './authMiddleware.js';
+//import authRoutes from './api/auth.js';
+import { initializeApp } from 'firebase-admin/app';
+
+import credentials from './config/piggybank-firebase-adminsdk.json?type=json';
+
+
+initializeApp({
+  credential: admin.credential.cert(credentials)
+});
+
+app.post('/register', async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await admin.auth().createUser({
+      email,
+      password
+    });
+    res.json({ message: 'User created successfully!', userId: user.uid });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Error creating user' });
+  }
+});
 
 // Create a new express application instance
 const app = express();
@@ -12,8 +34,10 @@ const PORT = process.env.PORT || 3000;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
-const registerRoute = authRoutes.register;
+app.use(express.urlencoded({extended: true}));
+
+//app.use('/api/auth', authRoutes);
+//const registerRoute = authRoutes.register;
 // Define a route
 app.get('/', (req, res) => {
   res.send('Hello PiggyBackend');
